9e0c6e3e92b82089f383196c7a8705cb082bf057
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-25 13:36:22 -0700

    modularize signin and signup middlewares

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL);
 app.use(express.urlencoded({ extended: true }));
 
 // Create a Sequelize model
-const Users = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+// const Users = sequelize.define('User', {
+//   username: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// });
 
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
